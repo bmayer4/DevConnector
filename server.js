f569776f13df866ef5624b32255130a90624536e
bmayer4 <bmayer55@yahoo.com>
2018-08-21 18:02:52 -0400

    prepared for deploy

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const keys = require('./config/keys');
 const passport = require('passport');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
@@ -30,6 +31,15 @@ require('./config/passport')(passport);
 app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
+
+//serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
 
 app.listen(port, () => {
   console.log(`server running on port ${port}`);
